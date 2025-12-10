daac3d8603a354a49badc57da067529c4692d016
Felipe <batatasteve304@gmail.com>
2025-12-10 19:05:55 -0300

    starting config

--- init.lua
+++ init.lua
@@ -1,1 +1,17 @@
 require("config.lazy")
+
+local set = vim.keymap.set
+
+set("n", "<space><space>x", "<cmd>source %<CR>") -- quick souce current file for when i'm editing my config
+set("n", "<space>w", ":w<CR>") -- quick save
+set("n", "<space>q", ":q<CR>") -- quick close
+set("n", "<space>x", ":x<CR>") -- quick save and close
+
+-- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd('TextYankPost', {
+	desc = 'Highlight when yanking (copying) text',
+	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
