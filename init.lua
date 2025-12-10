c3770a01f4cd41e4626678f1dcbe9532226fd515
Felipe <batatasteve304@gmail.com>
2025-12-10 20:08:42 -0300

    adding treesitter, formating existing files and removing unnecessary files

--- init.lua
+++ init.lua
@@ -7,11 +7,19 @@ set("n", "<space>w", ":w<CR>") -- quick save
 set("n", "<space>q", ":q<CR>") -- quick close
 set("n", "<space>x", ":x<CR>") -- quick save and close
 
+local opt = vim.opt
+
+opt.shiftwidth = 4
+opt.number = true
+opt.relativenumber = true
+opt.clipboard = "unnamedplus"
+
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd('TextYankPost', {
-	desc = 'Highlight when yanking (copying) text',
-	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
+
